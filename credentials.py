e1c29f95cbff2f602f113c7f789d9390648d491d
Chriswalt64 <walters.chris64@gmail.com>
2018-10-01 11:15:47 -0400

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,4 @@
+user = emailUsername@domain.domain
+passwd - emailPassword
+
+#This is where you can store your email credentials. I would recommend making an email specifically for this purpose, to avoid putting #your information at risk.
